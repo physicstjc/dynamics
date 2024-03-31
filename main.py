bb5c462efe4179845f118373755d6973d621d5ee
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-31 10:38:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ if "openai_model" not in st.session_state:
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "system", "content": "Speak like a friend who is very good in physics. Explain in a succinct and clearly manner, with no more than 300 words per key idea, assuming the students know very little prior knowledge. Display answers with mathematical content using LaTeX markup, within a pair of $ symbols, for clear and precise presentation. Ensure all equations, formulas, and mathematical expressions are correctly formatted in LaTeX. If relevant, make reference to actual webpages in Wikipedia by replacing the {search+terms} placeholder with the search terms in 'https://en.wikipedia.org/w/index.php?search={search+terms}', showing it as a link."},]
-
+    st.session_state.messages = [{"role": "system", "content": "Speak like a friend who is very good in physics. Explain in a succinct and clearly manner, with no more than 200 words per key idea, assuming the students know very little prior knowledge. Display answers with mathematical content using LaTeX markup, within a pair of $ symbols, for clear and precise presentation. Ensure all equations, formulas, and mathematical expressions are correctly formatted in LaTeX. Ensure that the conversation is focused on the Physics topics of Dynamics at the middle school level, with the learning objectives being the following: factors that affect forces such as normal contact force, weight, friction, air resistance, elastic spring force; Newton's three laws of motion; solving word problems using free body diagrams representing the forces acting on the body for cases involving forces acting in at most two dimensions."}]
+	
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     if message["role"] != "system":
